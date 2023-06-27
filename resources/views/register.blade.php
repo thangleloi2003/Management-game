d6fecc28f50012628e713397ee5c9a1718ca1edf
TonyStark-1505 <137202972+TonyStark-1505@users.noreply.github.com>
2023-06-27 11:35:49 +0700

    make register again

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.layout')
+{{-- @extends('layout.layout')
 @section('title', 'Register')
 @section('content')
 <form action="/register" method="post">
@@ -21,4 +21,59 @@
     </div>
     <button type="submit" class="btn btn-primary">Register</button>
 </form>
-@endsection
+@endsection --}}
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{URL('css/register.css')}}">
+    <!-- <link rel="stylesheet" href="login.css"> -->
+    <title>Document</title>
+</head>
+<style>
+
+</style>
+<body>
+    <form action="/register" method="post">
+        @csrf
+    <div class="box">
+        <div class="form">  
+        <h2>Sign up</h2>
+        <div class="inputBox">
+            <input type="text" name="name" required="required" id="name" >
+            <span>Full name</span>
+            <i></i>
+        </div>
+        <div class="inputBox">
+            <input type="text" name="email" required="required" id="email" >
+            <span>Email</span>
+            <i></i>
+        </div>
+        <div class="inputBox">
+            <input type="password" name="password" required="required" id="password">
+            <span>Password</span>
+            <i></i> 
+        </div>
+        <div class="inputBox">
+            <input type="password" name="password" required="required" id="password">
+            <span>Enter the password</span>
+            <i></i> 
+        </div>
+        <div class="links">
+            <a href="">Forgot password</a>
+            <a href="/login">Login</a>
+        </div>
+        <div class="input-field">
+            <input type="submit" class="submit" value="Signup" id="">
+        </div>
+        {{-- <input type="submit" value="Login"> --}}
+        @error('message')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+    </div>
+    </div>
+</form>
+</body>
+</html>
